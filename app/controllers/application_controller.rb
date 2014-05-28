c56c3505bdc821b0bb4c5d3af306f6999145b64b
erdnusskeks <erdnusskeks@googlemail.com>
2014-05-28 09:40:26 +0200

    Remove scaffold.css.scss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
+  # before_action :authenticate_user!   # that's what the tutorial said. but it doesn't work unless you comment it out.
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Railsgirls::Application.routes.draw do
-  resources :comments
+  resources :ideas
 
   root :to => redirect('/ideas')
-  resources :ideas
+  devise_for :users
+  resources :comments
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
